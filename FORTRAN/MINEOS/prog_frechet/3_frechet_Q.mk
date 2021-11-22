6ea85498ba70f8c77c0a381b5bf9ddad48714879
jbrussell <jbrussell93@gmail.com>
2021-11-22 11:25:56 -0500

    Add files required for calculating and plotting Q kernels

---
+++ FORTRAN/MINEOS/prog_frechet/3_frechet_Q.mk
@@ -0,0 +1,18 @@
+FBIN = /Users/russell/BROWN/RESEARCH/MINEOS_Q/MINEOS_synthetics/FORTRAN/bin
+FC = gfortran
+FFLAGS=-ffixed-line-length-none 
+#-L/usr/local/include 
+#FFLAGS2=-march=x86_64
+
+all:  $(FBIN)/frechet_Q 
+
+.f.o: 
+	$(FC) $(FFLAGS) $(FFLAGS2) -c $*.f
+
+#----------------------------------
+
+$(FBIN)/frechet_Q: frechet_Q.f
+	$(FC) $(FFLAGS) -o $(FBIN)/frechet_Q frechet_Q.f
+	
+clean: 
+	rm -rf *.o $(FBIN)/frechet_Q
