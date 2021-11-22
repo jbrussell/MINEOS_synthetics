501b85bdd51cfd7f128df8d29ea2adaf4c8d7d4f
jbrussell <jbrussell93@gmail.com>
2021-11-22 11:26:50 -0500

    update path

--- FORTRAN/MINEOS/prog_frechet/3_frechet_Q.mk
+++ FORTRAN/MINEOS/prog_frechet/3_frechet_Q.mk
@@ -1,4 +1,4 @@
-FBIN = /Users/russell/BROWN/RESEARCH/MINEOS_Q/MINEOS_synthetics/FORTRAN/bin
+FBIN = /Users/russell/Lamont/GITHUB/MINEOS_synthetics/FORTRAN/bin
 FC = gfortran
 FFLAGS=-ffixed-line-length-none 
 #-L/usr/local/include 
