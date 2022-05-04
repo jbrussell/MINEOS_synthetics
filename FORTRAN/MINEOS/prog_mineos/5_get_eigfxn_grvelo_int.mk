b572fc0f6f5fb8ace6d35f4af050874012fe1255
jbrussell <jbrussell93@gmail.com>
2022-05-04 18:04:26 -0400

    add new version of get_eigfxn_grvelo_int that accounts for water layer

--- FORTRAN/MINEOS/prog_mineos/5_get_eigfxn_grvelo_int.mk
+++ FORTRAN/MINEOS/prog_mineos/5_get_eigfxn_grvelo_int.mk
@@ -6,6 +6,6 @@ LFLAGS= -L$(MYLIB)
 
 
 #-------------------------------------------------------------------------------
-#f77 $(FFLAGS) $(LFLAGS) -o $(MYBIN)/get_eigfxn_grvelo get_eigfxn_grvelo.f
-get_eigfxn_grvelo: get_eigfxn_grvelo.o 
-	$(FC) $(FFLAGS) -o $(MYBIN)/get_eigfxn_grvelo get_eigfxn_grvelo.o
+#f77 $(FFLAGS) $(LFLAGS) -o $(MYBIN)/get_eigfxn_grvelo_int get_eigfxn_grvelo_int.f
+get_eigfxn_grvelo_int: get_eigfxn_grvelo_int.o 
+	$(FC) $(FFLAGS) -o $(MYBIN)/get_eigfxn_grvelo_int get_eigfxn_grvelo_int.o
